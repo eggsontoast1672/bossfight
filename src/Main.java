cb5434e20d2a3d3559a8ca8be460a2a08f3546a7
Paul Zupan <paul.zupan.13@gmail.com>
2021-05-28 22:26:20 -0700

    Added win condition checking to the main class

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,33 @@ package src;
 
 public class Main {
 
+  public static Boss boss = new Boss();
+  public static Player player = new Player();
+  public static Serum serum = new Serum();
+
+  public static boolean gameOver = false;
+
+  public static void checkWinConditions() {
+
+    if (boss.getHealth() <= 0) {
+      System.out.println("You land the killing blow, killing the boss. You emerge, victorious.");
+      gameOver = true;
+      return;
+    }
+
+    if (player.getHealth() <= 0) {
+      System.out.println("You are struck down by the boss. You aren't getting back up.");
+      gameOver = true;
+      return;
+    }
+
+  }
+
   public static void main(String[] args) {
+
+    while (!gameOver) {
+      // Game logic
+    }
 
   }
   
